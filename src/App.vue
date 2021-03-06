65e8a8076388dd73320ad82679a5d9cc9935d37b
chili <chili.bu@belstar.com.cn>
2021-03-06 13:24:25 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    首页
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/login.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import Login from './Login.vue'
+
+new Vue({
+  el: '#login',
+  render: h => h(Login)
+})
